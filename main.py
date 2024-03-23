000d8f9aa644ea6de901169d12dcab32ab1b241d
ІП-22 Христосенко Андрій <120760764+digitaleinc@users.noreply.github.com>
2024-03-23 18:57:05 +0200

    Remove comments

--- main.py
+++ main.py
@@ -37,14 +37,6 @@ def download_update(project_name: str, file_name: str, license_key: str):
                 raise HTTPException(status_code=404, detail="Requested object was not found")
             return FileResponse(path_to_file, media_type='application/octet-stream', filename=file_name)
         else:
-            # if is_blacklisted:
-            #     print(f"KEY {license_key} BLACKLISTED")
-            #     folder = pathlib.Path('empty')
-            #     path_to_file = folder / f'{file_name}'
-            #     with open(path_to_file, 'w') as f:
-            #         f.write('\n')
-            #     time.sleep(0.5)
-            #     return FileResponse(path_to_file, media_type='application/octet-stream', filename=file_name)
             raise HTTPException(status_code=404, detail="Requested object was not found")
     else:
         raise HTTPException(status_code=404, detail="Requested object was not found")
